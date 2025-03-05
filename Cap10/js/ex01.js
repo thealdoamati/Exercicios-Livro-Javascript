858b6cf9d1fd4b35cb1b6567be4171448fa177db
Aldo Amati <112902097+thealdoamati@users.noreply.github.com>
2025-03-05 11:28:16 -0300

    feat: função de gravar tarefas

--- Cap10/js/ex01.js
+++ Cap10/js/ex01.js
@@ -14,3 +14,27 @@ frm.addEventListener("submit", (e) => {
   frm.inTarefa.value = ""
   frm.inTarefa.focus()
 });
+
+
+frm.addEventListener("click", () => {
+  const tarefas = document.querySelectorAll("h5")
+
+  if(tarefas.length === 0) {
+    alert("Não há tarefas para serem salvas")
+    return
+  }
+
+  let dados = ""
+
+  tarefas.forEach(tarefa => {
+    dados += tarefa.innerText + ";"
+  })
+
+  // Salva removendo o último ";"
+  localStorage.setItem("tarefasDia", dados.slice(0, -1))
+
+  //confere se foi salvo
+  if(localStorage.getItem("tarefasDia")){
+    alert("Ok! Tarefas Salvas")
+  }
+})
