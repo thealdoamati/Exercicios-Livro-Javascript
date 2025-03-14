2f37a879b82d786cd99e9dab629f2a6b5e81cf96
Aldo Amati <aldilo08@hotmail.com>
2025-03-14 10:38:31 -0300

    upd: exercise

--- Cap10/js/ex03.js
+++ Cap10/js/ex03.js
@@ -1,28 +1,73 @@
-const frm = document.querySelector("form")
-const tbFilmes = document.querySelector("table")
+const frm = document.querySelector("form");
+const tbFilmes = document.querySelector("table");
 
 frm.addEventListener("submit", (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const titulo = frm.inTitulo.value
-    const genero = frm.inGenero.value
+  const titulo = frm.inTitulo.value;
+  const genero = frm.inGenero.value;
 
-    inserirLinha(titulo, genero)
-    gravarLinha(titulo, genero) // Salvando em localStorage
+  inserirLinha(titulo, genero);
+  gravarLinha(titulo, genero); // Salvando em localStorage
 
-    frm.reset()
-    frm.inTitulo.focus()
-
-})
+  frm.reset();
+  frm.inTitulo.focus();
+});
 
 const inserirLinha = (titulo, genero) => {
-    const linha = tbFilmes.insertRow(-1)
+  const linha = tbFilmes.insertRow(-1);
 
-    const col1 = linha.insertCell(0)
-    const col2 = linha.insertCell(1)
-    const col3 = linha.insertCell(2)
+  const col1 = linha.insertCell(0);
+  const col2 = linha.insertCell(1);
+  const col3 = linha.insertCell(2);
 
-    col1.innerText = titulo
-    col2.innerText = genero
-    col3.innerHTML = "<i class='exclui title='Excluir'>&#1008</i>"
-}
+  col1.innerText = titulo;
+  col2.innerText = genero;
+  col3.innerHTML = "<i class='exclui title='Excluir'>&#1008</i>";
+};
+
+const gravarLinha = (titulo, genero) => {
+  if (localStorage.getItem("filmesTitulo")) {
+    const filmesTitulo = localStorage.getItem("filmesTitulo") + ";" + titulo;
+    const filmesGenero = localStorage.getItem("filmesGenero") + ";" + genero;
+
+    localStorage.setItem("filmesTitulo", filmesTitulo);
+    localStorage.setItem("filmesGenero", filmesGenero);
+  } else {
+    localStorage.setItem("filmesTitulo", titulo);
+    localStorage.setItem("filmesGenero", genero);
+  }
+};
+
+tbFilmes.addEventListener("click", (e) => {
+  // se a classe no elemento alvo contém 'exclui'
+  if (e.target.classList.contains("exclui")) {
+    // acessa o pai do pai do elemento alvo, o obtem o texto do primeiro filho
+    const titulo = e.target.parentElement.parentElement.children[0].innerText;
+
+    if (confirm(`Confirmar exclusão de ${titulo}?`)) {
+      e.target.parentElement.parentElement.remove();
+
+      localStorage.removeItem("filmesTitulo");
+      localStorage.removeItem("filmesGenero");
+
+      // salvar novamente, se exister
+      for (let i = 1; i < tbFilmes.rows.length; i++) {
+        const auxTitulo = tbFilmes.rows[i].cells[0].innerText;
+        const auxGenero = tbFilmes.rows[i].cells[1].innerText;
+        gravarLinha(auxTitulo, auxGenero)
+      }
+    }
+  }
+});
+
+window.addEventListener("load", () => {
+  if (localStorage.getItem("filmesTitulo")) {
+    const filmesTitulo = localStorage.getItem("filmesTitulo").split(";");
+    const filmesGenero = localStorage.getItem("filmesGenero").split(";");
+
+    for (let i = 0; i < filmesTitulo.length; i++) {
+      inserirLinha(filmesTitulo[i], filmesGenero[i]);
+    }
+  }
+});
