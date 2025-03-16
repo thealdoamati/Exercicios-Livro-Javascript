8b5c83abd55b5f5e53858525192db8b3b66f75c0
Aldo Amati <112902097+thealdoamati@users.noreply.github.com>
2025-03-16 17:03:44 -0300

    new: exercise

---
+++ Cap10/js/ex04.js
@@ -0,0 +1,29 @@
+const frm = document.querySelector("form")
+const dvVelasRespose = document.querySelector("#divVelasRespose")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const idade = frm.inIdade.value
+
+    if(idade > 9) {
+        const digitos = idade.toString().split('')
+        digitos.forEach(digito => {
+            gerarImagens(digito)
+        })
+    } else {
+        gerarImagens(idade)
+    }
+
+    frm.submit.disabled = true
+})
+
+
+const gerarImagens = (num) => {
+    const novaImagem = document.createElement("img")
+    novaImagem.src = "img/numbers/number-" + num + ".png"
+    novaImagem.alt = "Vela número " + num
+    novaImagem.class = "vela" + num
+
+    dvVelasRespose.appendChild(novaImagem)
+}
