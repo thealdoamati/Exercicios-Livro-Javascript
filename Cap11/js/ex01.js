5cccf1bdd8c8833d89222e523daf9720ac4c1da1
Aldo Amati <112902097+thealdoamati@users.noreply.github.com>
2025-03-21 14:40:20 -0300

    new: exercise

--- Cap11/js/ex01.js
+++ Cap11/js/ex01.js
@@ -82,3 +82,48 @@ frm.inCavalo.addEventListener("focus", () => {
     frm.inCavalo.value = ""
     respCavalo.innerText = ""
 })
+
+frm.btResumo.addEventListener("click", () => {
+    const somaApostas = [0, 0, 0, 0, 0, 0]
+
+    for (const aposta of apostas) {
+        // - 1 por que começa com 0
+        somaApostas[aposta.cavalo - 1] += aposta.valor
+        console.log('somaApostas', somaApostas)
+    }
+
+    let resposta = `No Cavalo........... R$ Apostado\n${"-".repeat(35)}\n`
+    CAVALOS.forEach((cavalo, i) => {
+        resposta += `${i + 1} ${cavalo.padEnd(20)}`
+        resposta += `${somaApostas[i].toFixed(2).padStart(11)}\n`
+    })
+    respLista.innerText = resposta
+})
+
+frm.btGanhador.addEventListener("click", () => {
+    const ganhador = Number(prompt("No. Cavalo Ganhador"))
+
+    if (isNaN(ganhador) || validarCavalo(ganhador)){
+        alert("Cavalo Inválido")
+        return
+    }
+
+    const total = apostas.reduce((acumulador, aposta) => acumulador + aposta.valor, 0)
+
+    let resumo = `Resultado Final do Páreo\n${"-".repeat(30)}\n`
+    resumo += `No. Total de Apostas: ${apostas.length}\n`
+    resumo += `Total Geral R$: ${total.toFixed(2)}\n\n`
+    resumo += `Ganhador No ${ganhador} - ${obterCavalo(ganhador)}\n\n`
+    resumo += `No. de Apostas: ${contarApostas(ganhador)}\n`
+    resumo += `Total Apostado R$: ${totalizarApostas(ganhador).toFixed(2)}`
+
+    respLista.innerText = resumo
+
+    frm.btApostar.disabled = true
+    frm.btGanhador.disabled = true
+    frm.btNovo.focus()
+})
+
+frm.btNovo.addEventListener("click", () => {
+    window.location.reload()
+})
