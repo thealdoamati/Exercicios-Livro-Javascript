7e8371e6c5e4f7aa8197fba3bacaf67c7bbb5504
Aldo Amati <aldilo08@hotmail.com>
2025-04-09 12:00:32 -0300

    new: sorteio de palavra

---
+++ Cap11/js/ex03jogo.js
@@ -0,0 +1,43 @@
+const frm = document.querySelector("form")
+const respPalavra = document.querySelector("#outPalavra")
+const respErros = document.querySelector("#outErros")
+const respDica = document.querySelector("#outDica")
+const respChances = document.querySelector("#outChances")
+const rspMensagemFinal = document.querySelector("#outMensagemFinal")
+const imgStatus = document.querySelector("img")
+
+let palavraSorteada
+let dicaSorteada
+
+window.addEventListener("load", () => {
+    if(!localStorage.getItem("jogoPalavra")){
+        alert("Cadastre palavras para jogar")
+        frm.inLetra.disabled = true
+        frm.btJogar.disabled = true
+        frm.btVerDica.disabled = true
+    }
+
+    const palavras = localStorage.getItem("jogoPalavra").split(";")
+    const dicas = localStorage.getItem("jogoDica").split(";")
+
+    const tam = palavras.length
+
+    const numAleatorio = Math.floor(Math.random() * tam)
+
+    palavraSorteada = palavras[numAleatorio].toUpperCase()
+    dicaSorteada = dicas[numAleatorio]
+    let novaPalavra = ""
+
+    for (const letra of palavraSorteada) {
+        // se 
+        if(letra === palavraSorteada.charAt(0)){
+            novaPalavra += palavraSorteada.charAt(0)
+        } else {
+            novaPalavra += "_"
+        }
+    }
+
+    respPalavra.innerText = novaPalavra
+})
+
+
