2a17f6a4710eca12b1d3d8d4f1ae0584feacc7d0
Aldo Amati <aldilo08@hotmail.com>
2025-04-14 11:23:12 -0300

    upd: exercise

--- Cap12/app.js
+++ Cap12/app.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 3001;
 
 app.get("/", (req, res) => {
+  res.send("<h2>Olá... Bem-vindo!</h2>");
+});
+
+app.get("/cap12", (req, res) => {
   res.send("<h2>Capítulo 12: Introdução ao Express</h2>");
 });
 
