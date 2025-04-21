9383f4ac02f75ee318b222eb14fdf3c4dec88ce8
Aldo Amati <112902097+thealdoamati@users.noreply.github.com>
2025-04-21 14:11:42 -0300

    new: queries livros db

--- Cap12/livros.js
+++ Cap12/livros.js
@@ -13,7 +13,50 @@ router.get("/", async (req, res) => {
         res.status(400).json({ msg: error.message }) // retorna status de erro e msg 
     }
 })
-module.exports = router
+
+// Filtro por título ou autor
+router.get("/filtro/:palavra", async (req, res) => {
+    const palavra = req.params.palavra // palavra do titulo ou autor a pesquisar
+    try {
+        // para filtrar registros, usamos .where()
+        const livros = await dbKnex("livros")
+            .where("titulo", "like", `%${palavra}%`)
+            .orWhere("autor", "like", `%${palavra}%`)
+        res.status(200).json(livros)
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+})
+
+// Resumo do cadastro de livros
+router.get("/dados/resumo", async (req, res) => {
+    try {
+        // métodos para obter dados estatisticos da tabela
+        const consulta = await dbKnex("livros")
+            .count({ num: "*" })
+            .sum({ soma: "preco" })
+            .max({ maior: "preco" })
+            .avg({ media: "preco" })
+        const { num, soma, maior, media } = consulta[0]
+        res.status(200).json({ num, soma, maior, media: Number(media.toFixed(2)) })
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+})
+
+// Soma dos preços agrupados por ano
+router.get("/dados/grafico", async (req, res) => {
+    try {
+        // obtém ano e soma do preco dos livros (com nome total), agrupados por ano
+        const totalPorAno = await dbKnex("livros")
+            .select("ano")
+            .sum({ total: "preco" })
+            .groupBy("ano")
+        res.status(200).json(totalPorAno)
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+})
 
 // Método post é usado para inclusão
 router.post("/", async (req, res) => {
@@ -35,3 +78,31 @@ router.post("/", async (req, res) => {
         res.status(400).json({ msg: error.message })
     }
 })
+
+// Método put é usado para alteração. id indica o registro a ser alterado
+router.put("/:id", async (req, res) => {
+    const id = req.params.id // ou const { id } = req.params
+    const { preco } = req.body // campo a ser alterado
+
+    try {
+        // altera o campo preco, no registro cujo id coincidir com o parametro passado
+        await dbKnex("livros").update({ preco }).where("id", id)
+        res.status(200).json() // statusCode indica Ok
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+})
+
+// Método delete é usado para exclusão
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params
+
+    try {
+        await dbKnex("livros").del().where("id", id)
+        res.status(200).json()
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+})
+
+module.exports = router
