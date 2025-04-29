a0ff36665e214a967825bd753c6ec1b73fd9806e
Aldo Amati <aldilo08@hotmail.com>
2025-04-29 15:25:41 -0300

    upd: manutenção de livros

--- Cap13/src/components/ManutencaoLivros.js
+++ Cap13/src/components/ManutencaoLivros.js
@@ -1,12 +1,9 @@
-import { useState, useEffect } from "react";
-import { inAxios } from "../config_axios";
-import { useForm } from "react-hook-form";
-
 import ItemLista from "./ItemLista";
+import { useEffect, useState } from "react";
+import { inAxios } from "../config_axios";
 
 const ManutencaoLivros = () => {
   const [livros, setLivros] = useState([]);
-  const { register, handleSubmit, reset } = useForm();
 
   const obterLista = async () => {
     try {
@@ -17,48 +14,9 @@ const ManutencaoLivros = () => {
     }
   };
 
-  // define o método que será executado assim que o componente for renderizado
   useEffect(() => {
     obterLista();
   }, []);
-
-  const filtrarLista = async (campos) => {
-    try {
-      const lista = await inAxios.get(`livros/filtro/${campos.palavra}`);
-      lista.data.length
-        ? setLivros(lista.data)
-        : alert("Não há livros com a palavra-chave pesquisada...");
-    } catch (error) {
-      alert(`Erro... Não foi possível obter os dados: ${error}`);
-    }
-  };
-  
-  const excluir = async (id, titulo) => {
-    if (!window.confirm(`Confirma a exclusão do livro "${titulo}"?`)) {
-      return;
-    }
-    try {
-      await inAxios.delete(`livros/${id}`);
-      setLivros(livros.filter((livro) => livro.id !== id));
-    } catch (error) {
-      alert(`Erro... Não foi possível excluir este livro: ${error}`);
-    }
-  };
-
-  const alterar = async (id, titulo, index) => {
-    const novoPreco = Number(prompt(`Informe o novo preço do livro "${titulo}"`));
-    if (isNaN(novoPreco) || novoPreco === 0) {
-      return;
-    }
-    try {
-      await inAxios.put(`livros/${id}`, { preco: novoPreco });
-      const livrosAlteracao = [...livros]
-      livrosAlteracao[index].preco = novoPreco;
-      setLivros(livrosAlteracao);
-    } catch (error) {
-      alert(`Erro... Não foi possível alterar o preço: ${error}`);
-    }
-  };
 
   return (
     <div className="container">
@@ -67,13 +25,27 @@ const ManutencaoLivros = () => {
           <h4 className="fst-italic mt-3">Manutenção</h4>
         </div>
         <div className="col-sm-5">
-          <form onSubmit={handleSubmit(filtrarLista)}>
+          <form>
             <div className="input-group mt-3">
-              <input type="text" className="form-control" 
-                placeholder="Título ou autor" required {...register("palavra")} />
-              <input type="submit" className="btn btn-primary" value="Pesquisar" />
-              <input type="button" className="btn btn-danger" value="Todos" 
-                onClick={() => { reset({palavra: ""}); obterLista(); } } />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Título ou autor"
+                required
+              />
+              <input
+                type="submit"
+                className="btn btn-primary"
+                value="Pesquisar"
+              />
+              <input
+                type="button"
+                className="btn btn-danger"
+                value="Todos"
+                onClick={() => {
+                  obterLista();
+                }}
+              />
             </div>
           </form>
         </div>
@@ -82,7 +54,7 @@ const ManutencaoLivros = () => {
       <table className="table table-striped mt-3">
         <thead>
           <tr>
-            <th>Cód.</th> 
+            <th>Cód.</th>
             <th>Título</th>
             <th>Autor</th>
             <th>Ano</th>
@@ -93,10 +65,15 @@ const ManutencaoLivros = () => {
         </thead>
         <tbody>
           {livros.map((livro, index) => (
-            <ItemLista key={livro.id} id={livro.id} titulo={livro.titulo} 
-              autor={livro.autor} ano={livro.ano} preco={livro.preco} foto={livro.foto} 
-              excluirClick={() => excluir(livro.id, livro.titulo)} 
-              alterarClick={() => alterar(livro.id, livro.titulo, index)} />
+            <ItemLista
+              key={livro.id}
+              id={livro.id}
+              titulo={livro.titulo}
+              autor={livro.autor}
+              ano={livro.ano}
+              preco={livro.preco}
+              foto={livro.foto}
+            />
           ))}
         </tbody>
       </table>
